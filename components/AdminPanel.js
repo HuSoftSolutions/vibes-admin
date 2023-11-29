14fae2e0782451352907cf3d76d90def1bebbc66
codyhusek <cody.husek@husoftsolutions.com>
2023-11-29 01:21:11 -0500

    feat: updated sidebar nav

--- components/AdminPanel.js
+++ components/AdminPanel.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 
 // components/AdminPanel.js
 function AdminPanel() {
-	const [activeSection, setActiveSection] = useState('dashboard');
+	const [activeSection, setActiveSection] = useState('businesses');
 
   return (
 		<div className="flex h-screen bg-gray-100">
@@ -12,8 +12,8 @@ function AdminPanel() {
 				<div className="flex flex-col p-4">
 						<div className="text-lg font-semibold mb-4">Admin Panel</div>
 						<nav className="flex flex-col space-y-2">
-								<a href="#dashboard" className="text-gray-300 hover:text-white">Dashboard</a>
-								<a href="#users" className="text-gray-300 hover:text-white">Users</a>
+								{/* <a href="#dashboard" className="text-gray-300 hover:text-white">Dashboard</a> */}
+								{/* <a href="#users" className="text-gray-300 hover:text-white">Users</a> */}
                 <a href="#businesses" onClick={() => setActiveSection('businesses')} className="...">Businesses</a>
 
 								{/* Add more navigation links as needed */}
